4389a498e2ea42daf9118063fd5e8092e81ff405
morrozevich <150383856+morrozevich@users.noreply.github.com>
2024-11-10 23:37:32 +0100

    initital commit of onboarding navigation

---
+++ app/src/main/java/com/example/myapplicationnew/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myapplicationnew
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val loginText: TextView = findViewById(R.id.register)
+        loginText.setOnClickListener {
+            val intent = Intent(this, register_window::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
