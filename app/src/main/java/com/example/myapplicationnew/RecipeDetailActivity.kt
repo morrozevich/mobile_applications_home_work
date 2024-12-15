a068c79a69f67275d3d945c5c483fe2a1d0ca11a
bohhhdan <138617196+bohhhdan@users.noreply.github.com>
2024-12-15 23:25:53 +0100

    Recipe (or other) RecyclerView + PR

---
+++ app/src/main/java/com/example/myapplicationnew/RecipeDetailActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myapplicationnew
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class RecipeDetailActivity : AppCompatActivity() {
+    private lateinit var imageViewDetail: ImageView
+    private lateinit var textViewDetailName: TextView
+    private lateinit var textViewDetailDescription: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_detail)
+
+        imageViewDetail = findViewById(R.id.imageViewDetail)
+        textViewDetailName = findViewById(R.id.textViewDetailName)
+        textViewDetailDescription = findViewById(R.id.textViewDetailDescription)
+
+        val recipe = intent.getSerializableExtra("recipe") as Recipe
+        imageViewDetail.setImageResource(recipe.imageResource)
+        textViewDetailName.text = recipe.name
+        textViewDetailDescription.text = recipe.description
+    }
+}
